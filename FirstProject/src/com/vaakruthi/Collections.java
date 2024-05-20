e15fdddfa0cd44319bf894da153a91d9503712d0
dandusirisha25@gmail.com <91891@DESKTOP-SV3NUSL>
2024-05-20 12:02:00 +0530

    file modified

--- FirstProject/src/com/vaakruthi/Collections.java
+++ FirstProject/src/com/vaakruthi/Collections.java
@@ -5,10 +5,11 @@ class Collections {
 	public static void main (String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("Ravi");
-		list.add("siri");
+		list.add("siri");)
 		Iterator itr=list.iterator();  
 		while(itr.hasNext()){  
-		System.out.println(itr.next());  
+		System.out.println(itr.next()); 
+		System.out.println("hello");
 		}  
 	}
 }
